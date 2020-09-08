1b317b6846b223287dbe53ad83796914e893685f
Davide Fiocco <davide.fiocco@outlook.com>
2020-09-08 21:18:40 +0200

    more generous wait to allow the service to come up

--- indexer/indexer.py
+++ indexer/indexer.py
@@ -2,7 +2,7 @@ from elasticsearch import Elasticsearch
 from elasticsearch.helpers import bulk
 
 import time
-time.sleep(12)   # follow Mihai Todor's suggestion on https://stackoverflow.com/questions/48711455/create-dockerized-elasticsearch-index-using-a-python-script-running-in-docker/48712414#48712414
+time.sleep(30)   # follow Mihai Todor's suggestion on https://stackoverflow.com/questions/48711455/create-dockerized-elasticsearch-index-using-a-python-script-running-in-docker/48712414#48712414
 
 es = Elasticsearch(hosts=[{"host":'elasticsearch'}]) # what should I put here?
 
